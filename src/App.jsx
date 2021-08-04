4d0a5f4a68b6f0f6df5c0380387c18a1913dbed6
Bernard <bernardkrasowski111@gmail.com>
2021-08-04 20:10:31 +0100

    menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import './App.css';
+import './app.scss';
 import Topbar from './components/topbar/Topbar.jsx';
 import Intro from './components/intro/Intro.jsx';
 import Portfolio from './components/portfolio/Portfolio.jsx'
 import Works from './components/works/Works.jsx';
 import Testimonials from './components/testimonials/Testimonials.jsx';
 import Contact from './components/contact/Contact.jsx';
+import { useState } from 'react';
 
 function App() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="app">
-      <Topbar />
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <Portfolio />
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import './topbar.scss'
+import { Person, Mail } from "@material-ui/icons"
 
-export default function Toper() {
+export default function Toper({ menuOpen, setMenuOpen }) {
   return (
-    <div className='topbar'>
+    <div className={`topbar ${menuOpen && 'active'}`}>
+      <div className="wrapper ">
 
+        <div className="left">
+          <a href="#intro" className='logo'>genius.</a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+44 56541 65161</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>jakismail@gmial.com</span>
+          </div>
+        </div>
+
+
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+
+
+        </div>
+      </div>
     </div>
   )
 }
