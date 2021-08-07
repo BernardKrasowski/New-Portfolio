f1a441a30b246d337157583025d261c46c3060b4
Bernard <bernardkrasowski111@gmail.com>
2021-08-07 19:28:41 +0100

    contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import './contact.scss'
+import React from "react";
+import "./contact.scss";
+import imgContact from "../../images/contact.jpg";
 
 function Contact() {
   return (
-    <div className='contact' id="contact">
-
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src={imgContact} alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+        <form>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button>Send</button>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
