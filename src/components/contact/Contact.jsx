18b188694732c77b6d626ae40aa97b9d5bfb5373
Bernard <bernardkrasowski111@gmail.com>
2021-08-21 15:43:57 +0100

    contact page, submit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,18 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "./contact.scss";
 import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
 import { IconButton } from "@material-ui/core";
 
 function Contact({ menuOpen }) {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    const verificationEmail = checkEmail();
+    const verificationMessage = checkMessage();
+    if (verificationEmail && verificationMessage) {
+      const emailMessage = {
+        email,
+        message,
+      };
+      postEmailMessage();
+    } else {
+      !verificationMessage && alert(`You didn't write a message.`);
+      !verificationEmail && alert(`Wrong Email`);
+    }
+  };
+
+  const checkEmail = () => {
+    const index = [...email].findIndex((item) => item === "@");
+    return index != -1 ? true : false;
+  };
+  const checkMessage = () => {
+    return message ? true : false;
+  };
+  const postEmailMessage = () => {
+    console.log("email wyslany");
+  };
+
   return (
     <div className={`contact_wrapper ${menuOpen && "active"}`}>
       <div className="contact" id="contact">
         <h2>Contact</h2>
         <div className="img"></div>
-        <form>
-          <input type="text" placeholder="Email" />
-          <textarea placeholder="Message"></textarea>
-          <button>Send</button>
+        <form onSubmit={sendEmail}>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            placeholder="Email"
+          />
+          <textarea
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="Message"
+          ></textarea>
+          <button type="submit">Send</button>
         </form>
       </div>
       <a href="#intro">
